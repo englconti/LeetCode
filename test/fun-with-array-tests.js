bc0764635be0fc97092b042485c7aef6c883501e
LeonardoConti <leonardo.conti@luizalabs.com>
2022-08-26 11:19:34 -0300

    duplicate-zeros

--- test/fun-with-array-tests.js
+++ test/fun-with-array-tests.js
@@ -2,6 +2,7 @@ var assert = require('assert')
 const findNumbers = require('../fun-with-arrays/find-numbers-with-even-number-of-digits')
 const findMaxConsecutiveOnes = require('../fun-with-arrays/max-consecutive-ones')
 const sortedSquares = require('../fun-with-arrays/squares-of-a-sorted-array')
+const duplicateZeros = require('../fun-with-arrays/duplicate-zeros')
 
 describe('[MODULE] fun-with-array-tests', () => {
 
@@ -25,6 +26,21 @@ describe('[MODULE] fun-with-array-tests', () => {
             const unsortedArray = [-4, -1, 0, 3, 10]
             const answer = [ 0, 1, 9, 16, 100 ]
             assert.deepEqual(sortedSquares(unsortedArray), answer)
+        })
+    })
+
+    describe('[04]-duplicate-zeros', () => {
+        
+        it('[04-01] duplicateZeros should return [ 0, 1, 9, 16, 100 ] for [-4, -1, 0, 3, 10]', () => {
+            const inputArray = [1,0,2,3,0,4,5,0]
+            const expectedReturn = [1,0,0,2,3,0,0,4]
+            assert.deepEqual(duplicateZeros(inputArray), expectedReturn)
+        })
+
+        it('[04-02] duplicateZeros should return same array if it has no zeros', () => {
+            const inputArray = [1, 2, 3]
+            const expectedReturn = [1, 2, 3]
+            assert.deepEqual(duplicateZeros(inputArray), expectedReturn)
         })
     })
 })
